2928fda97e3a775bee1d981e3b6f95a2eb1f0bb3
xhw112 <2816947847@qq.com>
2024-03-31 20:47:54 +0800

    submit

--- 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
+++ 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
@@ -1,19 +1,23 @@
 package oy.tol.tra;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unused")
 public class QueueImplementation<E> implements QueueInterface<E> {
     private Object[] array;
     private int size = 0;
     private int capacity;
+    private int head = 0;
+    private int tail = -1;
 
     public QueueImplementation(int capacity) throws QueueAllocationException {
-        if (capacity < 1) {
-            throw new QueueAllocationException("Queue capacity must be at least 1");
+        if (capacity < 2) {
+            throw new QueueAllocationException("Queue capacity must be at least 2");
         }
         try {
             array = new Object[capacity];
         } catch (OutOfMemoryError e) {
-            throw new QueueAllocationException("Failed to allocate room for the internal array");
+            throw new QueueAllocationException("Failed room for the internal array");
         }
         this.capacity = capacity;
     }
@@ -35,29 +39,36 @@ public class QueueImplementation<E> implements QueueInterface<E> {
 
     @Override
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            array[i] = null;
-        }
+        Arrays.fill(array, null);
         size = 0;
+        head = 0;
+        tail = -1;
     }
 
     @Override
     public void enqueue(E element) throws QueueAllocationException, NullPointerException {
         if (element == null) {
-            throw new NullPointerException("Element cannot be null");
+            throw new NullPointerException("element can not be null");
         }
         if (size >= capacity) {
             int newCapacity = capacity * 2;
+            Object[] newArray;
             try {
-                Object[] newArray = new Object[newCapacity];
-                System.arraycopy(array, 0, newArray, 0, size);
+                newArray = new Object[newCapacity];
+                for (int i = 0; i < size; i++) {
+                    newArray[i] = array[(head + i) % capacity];
+                }
                 array = newArray;
                 capacity = newCapacity;
+                head = 0;
+                tail = size - 1;
             } catch (Exception e) {
                 throw new QueueAllocationException("Cannot allocate room for the internal array");
             }
         }
-        array[size++] = element;
+        tail = (tail + 1) % capacity;
+        array[tail] = element;
+        size++;
     }
 
     @SuppressWarnings("unchecked")
@@ -66,9 +77,10 @@ public class QueueImplementation<E> implements QueueInterface<E> {
         if (isEmpty()) {
             throw new QueueIsEmptyException("Queue is empty");
         }
-        E element = (E) array[0];
-        System.arraycopy(array, 1, array, 0, size - 1);
-        array[--size] = null;
+        E element = (E) array[head];
+        array[head] = null;
+        head = (head + 1) % capacity;
+        size--;
         return element;
     }
 
@@ -78,7 +90,7 @@ public class QueueImplementation<E> implements QueueInterface<E> {
         if (isEmpty()) {
             throw new QueueIsEmptyException("Queue is empty");
         }
-        return (E) array[0];
+        return (E) array[head];
     }
 
     @Override
@@ -86,7 +98,7 @@ public class QueueImplementation<E> implements QueueInterface<E> {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i < size; i++) {
-            sb.append(array[i]);
+            sb.append(array[(head + i) % capacity]);
             if (i < size - 1) {
                 sb.append(", ");
             }
